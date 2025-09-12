dc7ac3eca9cf0ae741c20dc1ff7e5e37a604354e
Rajesh TRiZ <67414041+rajeshrafaliyatriz@users.noreply.github.com>
2025-09-12 16:54:07 +0530

    bulk batch update : Message

--- resources/views/student/show_student_batch.blade.php
+++ resources/views/student/show_student_batch.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
         <div class="row bg-title">
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Bulk Student Batch</h4>
+                <h4 class="page-title">Bulk Batch Update</h4>
             </div>
         </div>
         @php
